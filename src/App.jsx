46c45b2499d3f51095484d16d9247ca6288c4d50
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-02-26 22:43:42 -0300

    create to do site

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, View } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './global.css'
+import { Header } from './components/header'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+    </div>
+    // <div className='mx-auto max-w-4xl my-20 px-6 py-3 bg-slate-700'>
+    //   <Header/>
+    // </div>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header className='flex bg-gray-900 items-center justify-center h-48 relative'>
+            <p className='font-bold text-3xl'>📘 To Do List</p>
+
+            {/* 📜📝 */}
+        </header>
+    )
+}
